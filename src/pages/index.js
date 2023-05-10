a8b075364be2b10520a837c479c2adc0a7b40cd9
Oskarnuz <oscar.nunez07@gmail.com>
2023-05-09 20:31:26 -0500

    Initial Commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Inicio - Mi Taller Automotriz</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Bienvenido a Mi Taller Automotriz</h1>
+        <p>Esta es la página de inicio de nuestra aplicación web.</p>
+      </main>
+    </div>
+  )
+}
